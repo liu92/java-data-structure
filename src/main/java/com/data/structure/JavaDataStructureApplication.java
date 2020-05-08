6aa5363afada99148bc1a46097e0fe3e4c88bd3e
liu92 <470437289@qq.com>
2020-05-08 22:08:41 +0800

    数据结构学习

---
+++ src/main/java/com/data/structure/JavaDataStructureApplication.java
@@ -0,0 +1,16 @@
+package com.data.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author h
+ */
+@SpringBootApplication
+public class JavaDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDataStructureApplication.class, args);
+    }
+
+}
